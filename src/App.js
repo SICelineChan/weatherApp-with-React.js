6940cffca2d360a01f3a3425bd513453f2ab31a3
Celine Chan <cctinlone@gmail.com>
2023-02-20 18:39:53 +0100

    built backbone to fetch Api data

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-import { React, useEffect, useState } from 'react';
-import './style.css';
+import { React, useState } from "react";
+import "./styles.css";
 
 export default function App() {
-  const myApiKey = 'fe9b9d62a21b37d39baab5ed769d8339';
+  const myApiKey = "fe9b9d62a21b37d39baab5ed769d8339";
   const [weatherInfo, setWeatherInfo] = useState([{}]);
   const [city, setCity] = useState([]);
 
-  useEffect(() => {
-    const getWeather = (event) => {
-      if (event.key == 'Enter') {
-        fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myApiKey}`
-        )
-          .then(function (res) {
-            return res.json();
-          })
-          .then(function (data) {
-            setWeatherInfo(data);
-            console.log(data);
-          });
-      }
-    };
-  }, []);
-}
+  const getWeather = (event) => {
+    if (event.key === "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myApiKey}&units=metric`
+      )
+        .then(function (res) {
+          return res.json();
+        })
+        .then(function (data) {
+          setWeatherInfo(data);
+          setCity("");
+          console.log(data);
+        });
+    }
+  };
 
-return (
-  <>
-    <div className="topContainer">
-      <h2> Hello and welcome to this weather app </h2>
-    </div>
-    <div className="inputBox1">
-      <input
-        className="input"
-        type="text"
-        placeholder="Enter a city"
-        onChange={(e) => setCity(e.target.value)}
-      />
-    </div>
-  </>
-);
+  return (
+    <>
+      <div className="topContainer">
+        <h2> Hello and welcome to this weather app </h2>
+      </div>
+      <div className="inputBox1">
+        <input
+          className="input"
+          type="text"
+          placeholder="Enter a city"
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={getWeather}
+        />
+
+        {typeof weatherInfo.main === "undefined" ? (
+          <div>
+            <p>Please enter in a city to get the weather information!</p>
+          </div>
+        ) : (
+          <div>
+            <p>{weatherInfo.name}</p>
+            <p>{weatherInfo.main.temp} Celsius </p>
+            <p>{weatherInfo.weather[0].main} </p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
