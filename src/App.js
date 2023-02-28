ead03fd3073fe3af4437076fa0b456bad6ac8fa2
SICelineChan <108868474+SICelineChan@users.noreply.github.com>
2023-02-28 12:01:08 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { React, useState } from "react";
-import "./styles.css";
+import React, { useState } from 'react';
+import './styles.css';
 
 export default function App() {
-  const myApiKey = "fe9b9d62a21b37d39baab5ed769d8339";
+  const myApiKey = 'fe9b9d62a21b37d39baab5ed769d8339';
   const [weatherInfo, setWeatherInfo] = useState([{}]);
   const [city, setCity] = useState([]);
 
   const getWeather = (event) => {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myApiKey}&units=metric`
       )
@@ -16,7 +16,7 @@ export default function App() {
         })
         .then(function (data) {
           setWeatherInfo(data);
-          setCity("");
+          setCity('');
           console.log(data);
         });
     }
@@ -24,29 +24,31 @@ export default function App() {
 
   return (
     <>
-      <div className="topContainer">
-        <h2> Hello and welcome to this weather app </h2>
+      <div className="main">
+        <div className="topContainer">
+          <h2> Hello and welcome to this weather app </h2>
 
-        <div className="inputBox1">
-          <input
-            className="input"
-            type="text"
-            placeholder="Enter a city"
-            onChange={(e) => setCity(e.target.value)}
-            onKeyDown={getWeather}
-          />
+          <div className="inputBox1">
+            <input
+              className="input"
+              type="text"
+              placeholder="Enter a city"
+              onChange={(e) => setCity(e.target.value)}
+              onKeyDown={getWeather}
+            />
 
-          {typeof weatherInfo.main === "undefined" ? (
-            <div>
-              <p>Please enter in a city to get the weather information!</p>
-            </div>
-          ) : (
-            <div>
-              <p>{weatherInfo.name}</p>
-              <p>{weatherInfo.main.temp} Celsius </p>
-              <p>{weatherInfo.weather[0].main} </p>
-            </div>
-          )}
+            {typeof weatherInfo.main === 'undefined' ? (
+              <div>
+                <p>Please enter in a city to get the weather information!</p>
+              </div>
+            ) : (
+              <div>
+                <p>{weatherInfo.name}</p>
+                <p>{weatherInfo.main.temp}&deg;C (Celsius) </p>
+                <p>{weatherInfo.weather[0].main} </p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
