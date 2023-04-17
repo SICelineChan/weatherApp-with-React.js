9844b8620cde39c0511bdd0567341570bd676970
Celine Chan <cctinlone@gmail.com>
2023-04-17 14:57:20 +0200

    update: a working digital clock

---
+++ src/TickingClock.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+export default function TickingClock() {
+  const [clock, setClock] = useState(new Date());
+
+  function refreshClock() {
+    setClock(new Date());
+  }
+  // without the cleanUp function, the clock still ticks!
+  useEffect(() => {
+    const clockId = setInterval(refreshClock, 1000);
+    return function cleanUp() {
+      clearInterval(clockId);
+    };
+  }, []);
+
+  const timeNow = new Date();
+  const timeInText = timeNow.toLocaleTimeString();
+
+  return (
+    <div>
+      <h1>Hello, we have two working clocks!</h1>
+      <p>This is the time now {timeInText}</p>
+      <p>The clock says {clock.toLocaleTimeString()}</p>
+    </div>
+  );
+}
